524d60f8e65ec675cd997699433d7f273e38fc92
ci <ci@pond>
2026-08-12 16:51:58 +0000

    pond 0.14.8

--- pkgs/pond/default.nix
+++ pkgs/pond/default.nix
@@ -2,12 +2,12 @@
 let
   inherit (stdenv) hostPlatform;
   inherit (hostPlatform) system;
-  version = "0.14.7";
+  version = "0.14.8";
   base = "https://github.com/tenequm/pond/releases/download/v${version}";
   shaMap = {
-    x86_64-linux = "684e62c697b972d20aeabfd6a68114d5e86fe2efe7e3dc59e2b708a097e8d036";
-    aarch64-linux = "4f9175927e5b7164d50a6c3b5db348bebf1702559c4a20dd22ec05672594bffb";
-    aarch64-darwin = "9b1404177f0a83eb4c5df3671897acc137348da332f00248190b9c7c8b0a6d6d";
+    x86_64-linux = "f4a0b49c12b494231af02b21bf80ae7e4bf081f6d42cb3a1cb30d94ed9b10e86";
+    aarch64-linux = "a29b07023f203397fe3783c7b64bd836a6b8ffba5aa99e95ec76819ce8af331d";
+    aarch64-darwin = "c7a9439a2c389681bd3f33ebbad33b5f904c2f7d4439384487f88a52df06d7e8";
   };
   urlMap = {
     x86_64-linux = "${base}/pond-x86_64-unknown-linux-gnu.tar.xz";
